bcdd3bdc2cab8f0f2683ee4c40144dddcd379c6a
wawanreg <wawanreg@gmail.com>
2018-12-22 10:58:38 +0100

    this is my first commit!

---
+++ OneIndonesia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OneIndonesia.Startup))]
+namespace OneIndonesia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
